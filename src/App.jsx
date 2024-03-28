ea4fbd40184b812621058bbf815d140b600a5519
CatherineZhou224 <cathzhou@umich.edu>
2024-03-28 01:44:44 -0400

    layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,95 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import React, { useState } from "react";
+import {
+  MenuFoldOutlined,
+  MenuUnfoldOutlined,
+  UserOutlined,
+  VideoCameraOutlined,
+  AndroidOutlined,
+  AppleOutlined,
+} from "@ant-design/icons";
+import { Layout, Menu, Button, Tabs, theme } from "antd";
+import Title from "antd/es/typography/Title";
 import "./App.css";
-
-function App() {
-  const [count, setCount] = useState(0);
+const { Header, Sider } = Layout;
+const App = () => {
+  const [collapsed, setCollapsed] = useState(false);
+  const {
+    token: { colorBgContainer },
+  } = theme.useToken();
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>PetLab</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <Layout style={{ minHeight: "100vh" }}>
+      <Sider trigger={null} collapsible collapsed={collapsed}>
+        <div className="demo-logo-vertical" />
+        <Menu
+          theme="dark"
+          mode="inline"
+          defaultSelectedKeys={["1"]}
+          items={[
+            {
+              key: "1",
+              icon: <UserOutlined />,
+              label: "Dog Collection",
+              children: [
+                {
+                  key: "1-1",
+                  label: "image1",
+                },
+              ],
+            },
+            {
+              key: "2",
+              icon: <VideoCameraOutlined />,
+              label: "Cat Collection",
+              children: [
+                {
+                  key: "2-1",
+                  label: "image1",
+                },
+              ],
+            },
+          ]}
+        />
+      </Sider>
+      <Layout>
+        <Header
+          style={{
+            display: "flex",
+            padding: 0,
+            background: colorBgContainer,
+          }}
+        >
+          <Button
+            type="text"
+            icon={collapsed ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />}
+            onClick={() => setCollapsed(!collapsed)}
+            style={{
+              fontSize: "16px",
+              width: 64,
+              height: 64,
+            }}
+          />
+          <Title
+            level={2}
+            style={{ alignSelf: "center", marginBottom: "revert" }}
+          >
+            Pet Lab
+          </Title>
+        </Header>
+        <Tabs
+          defaultActiveKey="2"
+          items={[AppleOutlined, AndroidOutlined].map((Icon, i) => {
+            const pet = ["Dog", "Cat"];
+            return {
+              key: i,
+              label: `${pet[i]}`,
+              children: `${pet[i]}`,
+              icon: <Icon />,
+            };
+          })}
+        />
+      </Layout>
+    </Layout>
   );
-}
-
+};
 export default App;
