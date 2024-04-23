108192c0b4eaaa603af78913376b32dd75b168d3
yixinsuewong <143902645+yixinsuewong@users.noreply.github.com>
2024-04-23 17:36:38 +0000

    loading_page comment

--- src/Components/Loading.jsx
+++ src/Components/Loading.jsx
@@ -4,34 +4,37 @@ import "../App.css";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import ProgressBar from 'react-bootstrap/ProgressBar';
 
+// Loading component that manages a progress bar animation and content display
 const Loading = ({ setProgress }) => { 
 
-    const [now, setNow] = useState(0);
-    const [fadeOut, setFadeOut] = useState(false); // State to trigger fade-out
+    const [now, setNow] = useState(0); // State for the current progress value
+    const [fadeOut, setFadeOut] = useState(false); // State to control fade-out effect on completion
 
     useEffect(() => {
-        document.body.classList.add('custom-body');
-        //fake progress bar with constant progressing speed
+        document.body.classList.add('custom-body'); // Add a custom class to the body for specific styles
+        // Set an interval to simulate progress
         const intervalId = setInterval(() => {
             setNow((prevNow) => {
-                const nextNow = prevNow + 5;
+                const nextNow = prevNow + 5; // Increment progress
                 if (nextNow >= 100) {
-                    clearInterval(intervalId);
-                    setFadeOut(true); // Start the fade-out
+                    clearInterval(intervalId); // Stop the interval when 100% is reached
+                    setFadeOut(true); // Initiate fade out effect
                     setTimeout(() => {
-                        setProgress(100);
-                    }, 500);
+                        setProgress(100); // Notify parent component that loading is complete
+                    }, 500); // Delay the completion notification to allow for fade-out effect
                     return 100;
                 }
-                return nextNow;
+                return nextNow; // Update progress
             });
-        }, 100);
+        }, 100); // Update every 100 milliseconds
 
+        // Cleanup function to clear interval on component unmount
         return () => {
             clearInterval(intervalId);
         };
     }, []);
 
+    // Determine the CSS class based on fadeOut state to apply fade-out effects
     const loadingStyle = fadeOut ? 'loading-content fade-out' : 'loading-content'; // Apply fade-out class
 
 
@@ -45,10 +48,10 @@ const Loading = ({ setProgress }) => {
                 className='custom-progress'
                 style={{ width: '50%', height: '25px', margin: '10px auto'}}
                 animated
-                now={now}
+                now={now} // Current progress
             />
 
-            <p className='loading-num'>{`${now}%`}</p>
+            <p className='loading-num'>{`${now}%`}</p> // Display numerical value of progress
         </div>
 ); }; 
 
