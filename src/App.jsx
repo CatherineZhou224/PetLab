79087c5e76f25005d5246bcd84e0db87541322b3
yixinsuewong <143902645+yixinsuewong@users.noreply.github.com>
2024-04-23 18:23:37 +0000

    indentation

--- src/App.jsx
+++ src/App.jsx
@@ -5,14 +5,11 @@ import MainContent from './Components/MainContent';
 import { getCatInfo } from "./utils/utils";
 // import bgm from "./src/assets/pet-lab-bgm.flac";
 
-
-
 const App = () => {
 
   // loading
   const [progress, setProgress] = useState(0);
-
-
+  
   return (progress !== 100) ? <Loading setProgress={setProgress} /> : <MainContent />;
 };
 
